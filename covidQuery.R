a880d631340089b67e9e4bdeddb16ef768787ef7
LoganPearce20 <112974521+LoganPearce20@users.noreply.github.com>
2023-05-04 20:08:01 -0500

    read in csv's to start cleaning

--- covidQuery.R
+++ covidQuery.R
@@ -1,1 +1,8 @@
+library(tidyverse)
+library(dplyr)
+library(magrittr)
  
+df_covid_data <- read.csv("data/Conditions_Contributing_to_COVID-19_Deaths__by_State_and_Age__Provisional_2020-2023.csv")
+df_income <- read.csv("data/income_by_state.csv")
+df_marital_status <- read.csv("data/marital_status.csv")
+
